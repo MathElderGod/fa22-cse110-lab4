cfcd76078e75a6f14dbc25a56fb7607539424211
Alexander G. Arias <agarias@ucsd.edu>
2022-10-19 10:33:31 -0700

    set up directories and practice js file.

---
+++ expose/javascript/practice.js
@@ -0,0 +1,14 @@
+"use strict"; // makes the var declaration mandatory
+let hello;
+hello = "Hello Alexander! :)";
+let message = "Java script is lit!"
+//let message = "Can I get an error?"; //leads to an error
+hello = message;
+const quickMaths = 10;
+console.log(hello);
+console.log(message); //alert(mesage) will not work, as this would only be for an html application
+console.log(quickMaths);
+// we should declare a variable once and then refer to it without let.
+// camelCaseIsKing :D
+// case sensitive
+// let, class, return, and function are reserved.
